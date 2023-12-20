e2fa09feb1c4dfc097f57a3047fc3fa780459bb0
Merseyside <ivanklessablin@gmail.com>
2023-12-20 17:37:30 +0600

    ++ver 1.1.0 Add cross compilation disabling

--- plugins/src/main/kotlin/com/merseyside/gradle/plugin/KotlinExtensionPlugin.kt
+++ plugins/src/main/kotlin/com/merseyside/gradle/plugin/KotlinExtensionPlugin.kt
@@ -7,8 +7,16 @@ import com.merseyside.gradle.printLog
 import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.publish.maven.tasks.AbstractPublishToMaven
+import org.gradle.api.publish.tasks.GenerateModuleMetadata
+import org.gradle.internal.os.OperatingSystem
 import org.gradle.kotlin.dsl.create
+import org.gradle.kotlin.dsl.get
+import org.gradle.kotlin.dsl.getByType
 import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.plugin.KotlinMultiplatformPluginWrapper
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 class KotlinExtensionPlugin : Plugin<Project> {
@@ -45,6 +53,64 @@ class KotlinExtensionPlugin : Plugin<Project> {
                     freeCompilerArgs = compilerArgs
                 }
             }
+
+            if (disableCrossCompilation) {
+                if (target.pluginManager.hasPlugin("kotlin-multiplatform")
+                    || target.pluginManager.hasPlugin("org.jetbrains.kotlin.multiplatform")
+                ) {
+
+                    target.plugins.withType(KotlinMultiplatformPluginWrapper::class) {
+                        val multiplatformExtension =
+                            target.extensions.getByType(KotlinMultiplatformExtension::class)
+
+                        disableCrossCompilation(target, multiplatformExtension)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun disableCrossCompilation(
+        project: Project,
+        kotlinMultiplatformExtension: KotlinMultiplatformExtension
+    ): Boolean {
+        with(project) {
+            with(kotlinMultiplatformExtension) {
+                val os = OperatingSystem.current()
+                val excludeTargets = when {
+                    !os.isMacOsX -> targets.filter { it.name.contains("ios") }
+                    else -> emptyList()
+                }.mapNotNull { it as? KotlinNativeTarget }
+
+                val message = if (excludeTargets.isNotEmpty()) {
+                    excludeTargets.joinToString(separator = ",", prefix = "Excluding targets: ")
+                } else {
+                    "No targets are going to be excluded..."
+                }
+
+                println(message)
+
+                configure(excludeTargets) { exclude ->
+                    exclude.compilations.all { compilation ->
+                        compilation.cinterops.all { cinterop ->
+                            tasks[cinterop.interopProcessingTaskName].enabled = false
+                        }
+                        compilation.compileTaskProvider.get().enabled = false
+                        tasks[compilation.processResourcesTaskName].enabled = false
+                    }
+                    exclude.binaries.all { binary -> binary.linkTask.enabled = false }
+
+                    exclude.mavenPublication {
+                        val publicationToDisable = this
+                        tasks.withType<AbstractPublishToMaven>()
+                            .all { task -> task.onlyIf { task.publication != publicationToDisable } }
+                        tasks.withType<GenerateModuleMetadata>()
+                            .all { task -> task.onlyIf { task.publication.get() != publicationToDisable } }
+                    }
+                }
+
+                return excludeTargets.isNotEmpty()
+            }
         }
     }
 }
@@ -52,6 +118,7 @@ class KotlinExtensionPlugin : Plugin<Project> {
 open class KotlinExtensionPluginExtension : LoggerExtension {
     var jvmTarget: JavaVersion = JavaVersion.VERSION_17
     var compilerArgs: List<String> = emptyList()
+    var disableCrossCompilation: Boolean = true
 
     fun setCompilerArgs(vararg args: String) {
         compilerArgs = args.toList()
